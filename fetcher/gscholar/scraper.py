2b54b211507751b936cdc205a40031c8fca39a3e
Dawid <dawid@dpago.dev>
2025-04-19 15:15:59 +0200

    Add Google Scholar scraper

---
+++ fetcher/gscholar/scraper.py
@@ -0,0 +1,20 @@
+from scholarly import scholarly
+from scholarly.data_types import Publication
+
+
+class GoogleScholarScraper:
+    # TODO: Turn scholarly into async
+    async def search(self, query: str) -> list[Publication]:
+        results = []
+        scholarly_query = scholarly.search_pubs(query)
+
+        try:
+            r = next(scholarly_query, 'end')
+            while r != 'end':
+                results.append(r)
+                r = next(scholarly_query, 'end')
+        except e:
+            print(f'GoogleScholarScraper errored out after scraping {len(results)} results')
+            print(e)
+        
+        return results
