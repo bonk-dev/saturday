489fd6ded74c00fb46247f396c038cf71e0fdfde
bonk-dev <github.p0j6t@passinbox.com>
2025-05-03 16:02:03 +0200

    Parse the search query from CLI args

--- main.py
+++ main.py
@@ -17,17 +17,21 @@ async def main():
     load_dotenv()
 
     parser = argparse.ArgumentParser(description="Science publication metadata scraper")
+    parser.add_argument('search_query', help='Generic search query to use when scraping metadata')
     parser.add_argument('-a', '--all', action='store_true', help='Use all methods (google-scholar, scopus)')
     parser.add_argument('-g', '--google-scholar', action='store_true', help='Use Google Scholar for scraping metadata')
     parser.add_argument('-s', '--scopus-api', action='store_true', help='Use Scopus API for scraping metadata')
     args = parser.parse_args()
+
+    search_query = args.search_query
+    print(f'Using "{search_query}" as the search query')
 
     use_scopus = args.scopus_api or args.all
     use_gscholar = args.google_scholar or args.all
 
     if use_gscholar:
         scr = GoogleScholarScraper(proxies=['http://127.0.0.1:8080'])
-        r = await scr.search('python3 c++ wulkan')
+        r = await scr.search(search_query)
         print(json.dumps(r))
         print(r)
 
@@ -40,7 +44,7 @@ async def main():
             return
 
         async with ScopusApi(api_key=scopus_key, api_endpoint=scopus_base) as client:
-            r = await client.search('python3')
+            r = await client.search(search_query)
             for entry in r:
                 print(entry)
 
