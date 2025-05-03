99f993795ba8dad03a07fea9e102bb95fa49db9e
bonk-dev <github.p0j6t@passinbox.com>
2025-05-03 16:06:20 +0200

    Use the stdlib logger in main.py

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ import json
 
 async def main():
     logging.basicConfig(level=logging.WARN)
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
 
     load_dotenv()
 
@@ -24,7 +26,7 @@ async def main():
     args = parser.parse_args()
 
     search_query = args.search_query
-    print(f'Using "{search_query}" as the search query')
+    logger.info(f'Using "{search_query}" as the search query')
 
     use_scopus = args.scopus_api or args.all
     use_gscholar = args.google_scholar or args.all
@@ -32,21 +34,21 @@ async def main():
     if use_gscholar:
         scr = GoogleScholarScraper(proxies=['http://127.0.0.1:8080'])
         r = await scr.search(search_query)
-        print(json.dumps(r))
-        print(r)
+        logger.debug(json.dumps(r))
+        logger.debug(r)
 
     if use_scopus:
         scopus_key = os.getenv('SCOPUS_API_KEY')
         scopus_base = os.getenv('SCOPUS_API_BASE')
 
         if scopus_base is None or scopus_base is None:
-            print("Please set SCOPUS_API_KEY and SCOPUS_API_BASE in .env (check out .env.sample) or with environment variables")
+            logger.critical("Please set SCOPUS_API_KEY and SCOPUS_API_BASE in .env (check out .env.sample) or with environment variables")
             return
 
         async with ScopusApi(api_key=scopus_key, api_endpoint=scopus_base) as client:
             r = await client.search(search_query)
             for entry in r:
-                print(entry)
+                logger.debug(entry)
 
         with app.app_context():
             init_app(app)
