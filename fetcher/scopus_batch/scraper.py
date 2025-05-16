a5684c11ec5e6a1ed72256f0dfb31934abd4f5ed
bonk-dev <github.p0j6t@passinbox.com>
2025-05-16 13:21:05 +0200

    scopus_batch: Increase timeout to 60 seconds

--- fetcher/scopus_batch/scraper.py
+++ fetcher/scopus_batch/scraper.py
@@ -37,7 +37,7 @@ class ScopusScraper:
 
     def __init__(self, config: ScopusScraperConfig, verify_ssl: bool = True):
         self.__BASE__ = 'https://www.scopus.com'
-        self._session = httpx.AsyncClient(verify=verify_ssl, timeout=30,
+        self._session = httpx.AsyncClient(verify=verify_ssl, timeout=60,
                                           cookies=config.build_cookie_store())
         self._session.headers.update({
             'Accept': 'application/json',
