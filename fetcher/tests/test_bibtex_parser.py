01025b182195bbf4939bfd0c653e10058d0472b6
bonk-dev <github.p0j6t@passinbox.com>
2025-05-26 20:12:37 +0200

    test(gscholar): add unit test for bibtex_parser.parse_bibtex_entry

--- fetcher/tests/test_bibtex_parser.py
+++ fetcher/tests/test_bibtex_parser.py
@@ -2,8 +2,9 @@ import json
 import os
 import unittest
 
-from fetcher.gscholar.bibtex_parser import merge_entries
-from fetcher.gscholar.models import GoogleScholarBibtexEntry, GoogleScholarHtmlEntry, GoogleScholarEntry
+from fetcher.gscholar.bibtex_parser import merge_entries, parse_bibtex_entry
+from fetcher.gscholar.models import GoogleScholarBibtexEntry, GoogleScholarHtmlEntry, GoogleScholarEntry, \
+    GoogleScholarBibtexScrapeEntry
 
 
 class TestBibTexParser(unittest.TestCase):
@@ -11,6 +12,23 @@ class TestBibTexParser(unittest.TestCase):
     def setUpClass(cls):
         base_path = os.path.dirname(__file__)
         cls.data_dir = os.path.join(base_path, 'data', 'bibtex-parser')
+
+    def test_parse_bibtex_entry(self):
+        entries_dir = os.path.join(self.data_dir, 'raw-entries')
+        for e in os.scandir(entries_dir):
+            if not e.name.endswith('.bib'):
+                continue
+            golden_file_path = e.path.removesuffix('.bib') + '.json'
+            with self.subTest(bib=e.name, json=e.name.replace('.bib', '.json')):
+                with open(e.path, 'r') as bibtex_file:
+                    bibtex_data = bibtex_file.read()
+                with open(golden_file_path, 'r') as golden_file:
+                    expected_data = GoogleScholarBibtexEntry(**json.load(golden_file))
+                bibtex_scrape_entry = GoogleScholarBibtexScrapeEntry(id=e.name.removesuffix('.bib'),
+                                                                     bibtex_data=bibtex_data)
+                actual_data = parse_bibtex_entry(bibtex_scrape_entry)
+                self.assertEqual(expected_data, actual_data,
+                                 msg='Parsed BibTex entry does not equal the expected entry')
 
     def test_merge_entries(self):
         bibtex_file_path = os.path.join(self.data_dir, 'bibtex-entries.json')
