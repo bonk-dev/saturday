f5f877371c7d1c184126e9941854a09353f5c8a8
bonk-dev <github.p0j6t@passinbox.com>
2025-06-11 12:11:21 +0200

    feat(docusaurus): set url and baseUrl to work with gh-pages

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://dpago.dev',
+  url: 'https://bonk-dev.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/saturday',
+  baseUrl: '/saturday/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
